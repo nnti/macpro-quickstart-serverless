7e494ddf79cd2b03bc85e6418f0349a41ad2d835
kenrick.veit <kenrick.veit@gmail.com>
2022-01-24 16:50:35 -0600

    Updated to pull out API gateway endpoint, not CloudFront

--- services/output.sh
+++ services/output.sh
@@ -2,7 +2,7 @@
 set -e
 
 help='This script is run with the format  ./output.sh <target service name> <serverless output variable name> <stage name (optional, default dev)>'
-example='ex.  ./output.sh ui CloudFrontEndpointUrl'
+example='ex.  ./output.sh app-api endpoints'
 
 : ${1?ERROR: 'You must specify the target service.'
 $help
@@ -15,8 +15,4 @@ service=${1}
 output=${2}
 stage=${3:-dev}
 
-if [ $output == "url" ]; then
-  output="CloudFrontEndpointUrl"
-fi
-
-cd $service && serverless info --stage $stage --verbose | sed -e '1,/^Stack Outputs/d' -e '$d' | sed -n -e "s/^.*$output: //p" && cd ..
+cd $service && serverless info --stage $stage --verbose | egrep -A 1 "$output:" && cd ..
