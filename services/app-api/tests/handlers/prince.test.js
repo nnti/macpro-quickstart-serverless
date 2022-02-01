8dcf63ba821708f536a8b3183a856d08b342828d
Berry Davenport <bdavenport@collabralink.com>
2022-02-01 14:19:28 -0500

    test

--- services/app-api/tests/handlers/prince.test.js
+++ services/app-api/tests/handlers/prince.test.js
@@ -39,16 +39,7 @@ describe("Test prince PDF convertor", () => {
     expect(response).toBeNull();
     expect(consoleLogMock).toHaveBeenCalledWith("Warmed up!");
   });
-/*
-  test("Verify error if text is not encoded correctly", async () => {
-    try {
-      const response = await prince({ body: output });
-    } catch(e) {
-      expect(e instanceof Error).toBe(true);
-      expect(e.message).toEqual("Content is not base64 encoded.");
-    }
-  });
-*/
+
   test("Verify text is encoded correctly", async () => {
     const response = await prince({ body: encodedInput });
     expect(writeFileSyncMock).toHaveBeenCalledWith("/tmp/input", output);
