f4b4f3e62fc36f6436b35e7b53f3a87ac4f3d2c9
Berry Davenport <bdavenport@collabralink.com>
2022-02-01 09:32:38 -0500

    test

--- services/app-api/handlers/prince.js
+++ services/app-api/handlers/prince.js
@@ -1,27 +1,34 @@
 import handler from "./../libs/handler-lib";
-//var execSync = require("child_process").execSync;
-//const fs = require("fs");
 import {execSync} from "child_process";
 import * as fs from "fs";
 
 export const prince = async (event, context) => {
+  if (!event || !event.body) {
+    return new Error("No data.");
+  }
+
   // If this invocation is a prewarm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
     return null;
   }
-  if (!event || !event.body) {
-    return new Error("No data.");
+
+  const body = event.body;
+  const b64EncodedString = Buffer.from(body, "base64");
+
+  //verify input is base64 encoded
+  if(b64EncodedString.toString('base64') !== body) {
+    return new Error("Content is not base64 encoded.");
   }
-  let body = event.body;
-  let html = Buffer.from(body, "base64").toString("ascii");
+
+  let html = b64EncodedString.toString("ascii");
   fs.writeFileSync("/tmp/input", html);
   try {
-    // ["-", "-o", "-", "--pdf-profile=PDF/UA-1"],
     let result = execSync(`/opt/prince /tmp/input -o - --pdf-profile=PDF/UA-1`);
     return result.toString("base64");
   } catch (err) {
     console.log(err);
+    return new Error(err);
   }
 };
 
--- services/app-api/tests/handlers/Prince.test.js
+++ services/app-api/tests/handlers/Prince.test.js
@@ -1,7 +1,54 @@
-const prince = require('../../handlers/prince');
+import {prince} from "../../handlers/prince";
+import * as child_process from "child_process";
+import * as fs from "fs";
+
+jest.mock("child_process");
+jest.mock("fs");
 
 describe("Test prince PDF convertor", () => {
-  test("Validate PDF", () => {
-    expect(true).toEqual(true);
+  beforeEach(() => {
+    jest.restoreAllMocks();
   });
+
+  const output = "ABC";
+  const encodedInput = "QUJD";
+  const execSyncMock = jest.spyOn(child_process,'execSync').mockImplementation(() => output);
+  const writeFileSyncMock = jest.spyOn(fs, "writeFileSync").mockImplementation(() => {});
+
+  test("Verify empty event returns error", async() => {
+    const response = await prince({source: null});
+    expect(response instanceof Error).toBe(true);
+    expect(response.message).toEqual("No data.");
+  });
+
+  test("Verify serverless warmup ejection", async () => {
+    const response = await prince({source: "serverless-plugin-warmup", body: {}});
+    expect(response).toBeNull();
+  });
+
+  test("Verify error if text is not encoded correctly", async () => {
+    const response = await prince({body: output});
+    expect(response instanceof Error).toBe(true);
+    expect(response.message).toEqual("Content is not base64 encoded.");
+  });
+
+  test("Verify text is encoded correctly", async () => {
+    const response = await prince({body: encodedInput});
+    expect(writeFileSyncMock).toHaveBeenCalledWith("/tmp/input",output);
+  });
+
+  test("Verify execSync is executed correctly", async () => {
+    const response = await prince({body: encodedInput});
+    expect(execSyncMock).toHaveBeenCalledWith('/opt/prince /tmp/input -o - --pdf-profile=PDF/UA-1');
+    expect(response).toEqual(output);
+  });
+
+  test("Verify error is returned if execSync returns something other than a string", async () => {
+    execSyncMock.mockImplementation(() => null);
+    const consoleMock = jest.spyOn(console, 'log').mockImplementation(() => {});;
+    const response = await prince({body: encodedInput});
+    expect(response instanceof Error).toBe(true);
+    expect(response.message).toEqual("TypeError: Cannot read property 'toString' of null");
+  });
+
 });
